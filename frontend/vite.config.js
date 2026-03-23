4c64a0fe85951955f7b816f4fdfa34006b26f66c
Cristina2M <cmormar2509@g.educaand.es>
2026-03-23 18:46:02 +0100

    frontendV1 - configuracion de Vite para recargar cambios automaticamente en Docker Windows

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Necesario para que funcione desde dentro del contenedor
+    port: 5173
+  }
 })
